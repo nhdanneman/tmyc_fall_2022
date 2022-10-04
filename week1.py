f9f0f0043bff2707febdc42ea0885340bcb03bf8
Nathan Danneman <ndanneman@gmail.com>
2022-10-03 21:04:27 -0400

    adding more, and more helpful, comments

--- week1.py
+++ week1.py
@@ -69,20 +69,28 @@ make the value at index 0, after the program ends, 19690720.
 '''
 
 def tester(original):
+    # range makes a set of integers, by default, from 0 up to 1-arg.
     for i in range(100):
         for j in range(100):
+            # Note: you don't want to ALTER the original each time.
+            # Using copy.deepcopy makes a totally new copy (literally on your actual computer's memory)
             working = copy.deepcopy(original)
             working[1] = i
             working[2] = j
             output = intcodeSolver(working, 0)
+            # check for completion condition
             if output[0] == 19690720:
                 print(i)
                 print(j)
+                # All we need is to print the answer, so let's just do that!
                 ans = 100 * i + j
                 msg = "Answer is " + str(ans)
                 print(msg)
                 break
 
+# recopied this down here, because don't want the altered version from above
+# as my original function ALTERED THE ORIGINAL EVERY TIME
 week1_input = [1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,2,19,6,23,2,13,23,27,1,9,27,31,2,31,9,35,1,6,35,39,2,10,39,43,1,5,43,47,1,5,47,51,2,51,6,55,2,10,55,59,1,59,9,63,2,13,63,67,1,10,67,71,1,71,5,75,1,75,6,79,1,10,79,83,1,5,83,87,1,5,87,91,2,91,6,95,2,6,95,99,2,10,99,103,1,103,5,107,1,2,107,111,1,6,111,0,99,2,14,0,0]
 
+# We run this, hopefully we get answers!
 tester(week1_input)
